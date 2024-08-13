af70104fcd8ce536d12e13e5008f9119a5548372
Finlay Smith <161974589+fxnners@users.noreply.github.com>
2024-08-13 18:02:00 +0100

    Update User.java

--- User.java
+++ User.java
@@ -2,12 +2,28 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
 
-
 public class User {
-  public String name;
-  public LocalDate birthDate;
-  public ArrayList<Book> books = new ArrayList<Book>();
+  private String name;
+  private LocalDate birthDate;
+  private ArrayList<Book> books = new ArrayList<Book>();
 
+  User(String name, String birthDate) {
+    this.name = name;
+    this.birthDate = LocalDate.parse(birthDate);
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getBirthDate() {
+    return this.birthDate.toString();
+  }
+
+  public String borrowedBooks() {
+    return this.books.toString();
+  }
+  
   public void borrow(Book book) {
     this.books.add(book);
   }

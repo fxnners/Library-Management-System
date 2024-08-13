aea47999bbc2d457f91b67d7f4303860c62b55f0
Finlay Smith <161974589+fxnners@users.noreply.github.com>
2024-08-13 18:02:34 +0100

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,6 +1,21 @@
 public class Book {
-  public String title;
-  public String writer;
+  private String title;
+  private String writer;
+  private int pageCount;
+
+  Book(String title, String writer, int pageCount) {
+    this.title = title;
+    this.writer = writer;
+    this.pageCount = pageCount;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public String getWriter() {
+    return this.writer;
+  }
 
   public String toString() {
     return String.format("%s by %s", this.title, this.writer);
